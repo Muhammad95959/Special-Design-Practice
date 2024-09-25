a4aa22de439026608730efa9a02e7f2680b93f4a
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-09-25 06:38:20 +0300

    add skills section

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
 const menuBtn = document.querySelector(".menu-btn");
 const links = document.querySelector(".links");
 
+// toggle menu visibility with a fade effect
 menuBtn.onclick = function () {
   if (links.classList.contains("show")) {
-    links.style.opacity = 0;
+    links.classList.remove("opacity-1");
     setTimeout(() => {
-      links.style.opacity = 1;
       links.classList.remove("show");
     }, 300);
   } else {
-    links.style.opacity = 1;
+    links.classList.add("opacity-1");
     links.classList.add("show");
   }
+};
+
+// close menuBtn after clicking outside the menu and its icon
+document.addEventListener("click", function (e) {
+  if (
+    e.target.parentNode !== menuBtn &&
+    e.target !== menuBtn &&
+    e.target !== links &&
+    links.classList.contains("show")
+  ) {
+    links.classList.remove("opacity-1");
+    setTimeout(() => {
+      links.classList.remove("show");
+    }, 300);
+  }
+});
+
+// show skills progress after reaching the section
+window.onscroll = function () {
+  document.querySelectorAll(".skill .progress-bar > div").forEach(function (e) {
+    const elementHeight = e.offsetHeight;
+    const elementOffset = e.offsetTop;
+    const windowHeight = window.innerHeight;
+    const windowScroll = window.scrollY;
+    if (windowScroll > elementHeight + elementOffset - windowHeight)
+      e.style.transform = "translateX(0)";
+  });
 };
